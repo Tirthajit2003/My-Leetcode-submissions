28c58fad970e4213573866866b49bcb5d719caae
Tirthajit Boral <121869956+Tirthajit2003@users.noreply.github.com>
2024-05-14 23:58:56 +0530

    Time: 60 ms (87.54%) | Memory: 40.9 MB (69.45%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int find(int i,int j,int[][] grid)
+    {
+        if (i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]==0) 
+            return 0;
+        
+        int curr=grid[i][j];
+        grid[i][j]=0;
+        int res=0;
+
+        int left=find(i,j-1,grid);
+        int right=find(i,j+1,grid);
+        int up=find(i-1,j,grid);
+        int down=find(i+1,j,grid);
+
+        res=Math.max(res,curr+Math.max(left,Math.max(right,Math.max(up,down))));
+
+        grid[i][j]=curr;
+
+        return res;
+
+    }
+    public int getMaximumGold(int[][] grid) {
+        int m=grid.length,n=grid[0].length;
+        int res=0;
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if (grid[i][j]!=0)
+                    res=Math.max(res,find(i,j,grid));
+            }  
+        }
+        return res;
+        
+    }
+}
