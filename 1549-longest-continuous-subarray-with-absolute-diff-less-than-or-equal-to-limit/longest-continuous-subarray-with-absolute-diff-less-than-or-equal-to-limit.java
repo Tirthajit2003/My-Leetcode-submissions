76f704d0be1dcb38a96774afffdc32c563b1e3b0
Tirthajit Boral <121869956+Tirthajit2003@users.noreply.github.com>
2024-06-23 17:21:33 +0530

    Time: 73 ms (34.55%) | Memory: 56.3 MB (69.84%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -3,23 +3,23 @@ import java.util.TreeMap;
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
         TreeMap<Integer, Integer> maxMap = new TreeMap<>();
-        TreeMap<Integer, Integer> minMap = new TreeMap<>();
+        //TreeMap<Integer, Integer> minMap = new TreeMap<>();
         int s = 0, res = 0;
 
         for (int i = 0; i < nums.length; i++) {
             maxMap.put(nums[i], maxMap.getOrDefault(nums[i], 0) + 1);
-            minMap.put(nums[i], minMap.getOrDefault(nums[i], 0) + 1);
+            //minMap.put(nums[i], minMap.getOrDefault(nums[i], 0) + 1);
 
-            while (maxMap.lastKey() - minMap.firstKey() > limit) {
+            while (maxMap.lastKey() - maxMap.firstKey() > limit) {
                 maxMap.put(nums[s], maxMap.get(nums[s]) - 1);
                 if (maxMap.get(nums[s]) == 0) {
                     maxMap.remove(nums[s]);
                 }
 
-                minMap.put(nums[s], minMap.get(nums[s]) - 1);
-                if (minMap.get(nums[s]) == 0) {
-                    minMap.remove(nums[s]);
-                }
+                // minMap.put(nums[s], minMap.get(nums[s]) - 1);
+                // if (minMap.get(nums[s]) == 0) {
+                //     minMap.remove(nums[s]);
+                // }
 
                 s++;
             }
