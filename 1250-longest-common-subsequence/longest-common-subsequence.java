07438b34c3213a364a784c028f18c6cf310ac84d
Tirthajit Boral <121869956+Tirthajit2003@users.noreply.github.com>
2024-08-05 10:35:02 +0530

    Time: 20 ms (73.56%) | Memory: 50.5 MB (75.44%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,24 +1,34 @@
 class Solution {
-    public int find(int i,int j,String text1,String text2,int[][] dp)
-    {
-        if (i==text1.length() || j==text2.length()) return 0;
-        if (dp[i][j]!=-1) return dp[i][j];
+    // public int find(int i,int j,String text1,String text2,int[][] dp)
+    // {
+    //     if (i==text1.length() || j==text2.length()) return 0;
+    //     if (dp[i][j]!=-1) return dp[i][j];
 
-        if (text1.charAt(i)==text2.charAt(j))
-            return 1+find(i+1,j+1,text1,text2,dp);
+    //     if (text1.charAt(i)==text2.charAt(j))
+    //         return 1+find(i+1,j+1,text1,text2,dp);
 
-        return dp[i][j]=Math.max(find(i+1,j,text1,text2,dp),find(i,j+1,text1,text2,dp));
-    }
+    //     return dp[i][j]=Math.max(find(i+1,j,text1,text2,dp),find(i,j+1,text1,text2,dp));
+    // }
     public int longestCommonSubsequence(String text1, String text2) {
-        int n=text1.length();
-        int m=text2.length();
-
-        int dp[][]=new int[n][m];
-
-        for(int[] r:dp)
-            Arrays.fill(r,-1);
+        int n = text1.length();
+        int m = text2.length();
         
-        int res=find(0,0,text1,text2,dp);
-        return res;
+        // Create a dp array with all elements initialized to 0
+        int[][] dp = new int[n + 1][m + 1];
+        
+        // Fill the dp array
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        // The result is in dp[n][m]
+        return dp[n][m];
     }
 }
+
